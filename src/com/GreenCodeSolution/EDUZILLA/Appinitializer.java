74aa47e1d608a6242c25c2a2eae507e885f67148
tcdruberu <94827905+tcdruberu@users.noreply.github.com>
2023-05-10 20:58:33 +0530

    Initial commit

---
+++ src/com/GreenCodeSolution/EDUZILLA/Appinitializer.java
@@ -0,0 +1,23 @@
+package com.GreenCodeSolution.EDUZILLA;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Appinitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // path => (relative, absolute)
+        primaryStage.setScene(
+                new Scene(FXMLLoader.load(getClass()
+                        .getResource("view/LoginForm.fxml"))));
+        primaryStage.show();
+    }
+}
